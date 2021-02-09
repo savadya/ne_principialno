0e5598818b361b8c68cf484b2550a0a3856f67af
sava <savadyagilev@yandex.ru>
2021-02-09 18:55:58 +0300

    task 5

--- task_1.py
+++ task_1.py
@@ -3,14 +3,16 @@ import sys
 import math
 import pygame
 import requests
+from yandex_geocoder import Client
 
 coord = ['37.628070', '55.750630']
 height, width = 600, 450
 scale = 15
+client = Client("40d1649f-0493-4b70-98ba-98533de7710b")
 
 
 def update_image():
-    map_request = f"https://static-maps.yandex.ru/1.x/?ll={coord[0]},{coord[1]}&z={scale}&size={height},{width}&l=map"
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll={coord[0]},{coord[1]}&z={scale}&size={height},{width}&l=map&pt={coord[0]},{coord[1]}"
 
     response = requests.get(map_request)
     if not response:
@@ -26,18 +28,58 @@ def update_image():
 
     return map_file
 
+def get_coord(address):
+    coordinates = client.coordinates(address)
+    return coordinates
+
+class Button:
+    def __init__(self, x, y, height, width, text):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.text = text
+        self.draw()
+
+    def draw(self):
+        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.height, self.width), width=1)
+
+        font = pygame.font.Font(None, self.width)
+        text = font.render(self.text, True, (255, 255, 255))
+        screen.blit(text, (self.x + 15, self.y + 5))
+
+    def push_button(self, pos):
+        if pos[0] > self.x and pos[0] < self.x + self.height and pos[1] > self.y and pos[1] < self.y + self.width:
+            return True
+
 
 pygame.init()
-screen = pygame.display.set_mode((height, width))
+screen = pygame.display.set_mode((height, width + 200))
 map_file = update_image()
+button = Button(250, 600, 100, 30, 'Искать')
+font = pygame.font.Font(None, 30)
 
 pygame.display.flip()
 run = True
+address = ''
+text = font.render(address, True, (255, 255, 255))
+rect = text.get_rect()
+rect.center = (150, 550)
 
 while run:
+    screen.fill((0, 0, 0))
+    button.draw()
+    pygame.draw.line(screen, (255, 255, 255), (150, 550), (450, 550))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button.push_button(event.pos):
+                coord = get_coord(address)
+                update_image()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_PAGEUP:
                 if scale < 17:
@@ -60,6 +102,20 @@ while run:
                 if event.key == pygame.K_LEFT:
                     coord[0] = str(float(coord[0]) - 0.008 * math.pow(2, 15 - scale))
                     map_file = update_image()
-            print(scale)
+
+                if event.unicode:
+                    address += event.unicode
+                elif event.key == pygame.K_BACKSPACE:
+                    address = address[:-1]
+                elif event.key == pygame.K_DELETE:
+                    address = ""
+
+                text = font.render(address, True, (255, 255, 255))
+                rect = text.get_rect()
+                rect.center = (300 + len(address) * 0.1, 535)
+                
+    screen.blit(text, rect)
+            
     pygame.display.flip()
+os.remove(map_file)
 pygame.quit()
